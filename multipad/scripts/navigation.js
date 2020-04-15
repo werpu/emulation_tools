18b9cccb2c383788ad531a92f97d974e71b39474
Werner Punz <werner.punz@gmail.com>
2020-04-15 23:27:36 +0200

    smaller layout fixes

--- multipad/scripts/navigation.js
+++ multipad/scripts/navigation.js
@@ -1,5 +1,5 @@
 try {
-// Or use an anonymous class if you don't want a named constructor in current scope.
+    // Or use an anonymous class if you don't want a named constructor in current scope.
     window.customElements.define('x-navigation', class extends HTMLElement {
 
         selected;
@@ -9,20 +9,23 @@ try {
 
             this.innerHTML = "<button id='nav_c64' data-target='c64'>C64</button><button id='atari5200' data-target='atari5200'>Atari 5200</button><button id='coleco-1p' data-target='coleco-1p'>Colecovision</button>";
 
-            DomQuery.querySelectorAll("x-navigation button").addEventListener("click", (evt) => {
-                let target = evt.target.dataset["target"];
-                location.href = target+'.html';
-            });
+
 
         }
 
         connectedCallback() {
-            if (!this.selected) {
-                this.selected = "c64";
-            }
+
+            DomQuery.querySelectorAll("x-navigation button").addEventListener("click", (evt) => {
+                let target = evt.target.dataset["target"];
+                location.href = target+'.html';
+
+            }).each((item => {
+                item.removeClass("hidden");
+                if(item.attr("data-target").value == DomQuery.byId(this).attr("selected").value) {
+                    item.addClass("hidden");
+                }
+            }));
         }
-
-
     });
 } catch (e) {
     alert(e.message)
