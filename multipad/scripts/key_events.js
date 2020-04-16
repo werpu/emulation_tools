5033b15ecd510bad02d08a52e1d03e14c643ab66
Werner Punz <werner.punz@gmail.com>
2020-04-16 17:09:57 +0200

    adding auto registration of keycodes to reduce boilerplate code

---
+++ multipad/scripts/key_events.js
@@ -0,0 +1,134 @@
+class KeyCodes {
+    static KEY_ESC = "(EV_KEY), code 1 (KEY_ESC)";
+
+    /*function keys*/
+    static KEY_F1 = "(EV_KEY), code 59 (KEY_F1)";
+    static KEY_F2 = "(EV_KEY), code 60 (KEY_F2)";
+    static KEY_F3 = "(EV_KEY), code 61 (KEY_F3)";
+    static KEY_F4 = "(EV_KEY), code 62 (KEY_F4)";
+    static KEY_F5 = "(EV_KEY), code 63 (KEY_F5)";
+    static KEY_F6 = "(EV_KEY), code 64 (KEY_F6)";
+    static KEY_F7 = "(EV_KEY), code 65 (KEY_F7)";
+    static KEY_F8 = "(EV_KEY), code 66 (KEY_F8)";
+    static KEY_F9 = "(EV_KEY), code 67 (KEY_F9)";
+    static KEY_F10 = "(EV_KEY), code 68 (KEY_F10)";
+    static KEY_F11 = "(EV_KEY), code 87 (KEY_F11)";
+    static KEY_F12 = "(EV_KEY), code 88 (KEY_F12)";
+
+    /*number keys*/
+    static KEY_1 = "(EV_KEY), code 2 (KEY_1)";
+    static KEY_2 = "(EV_KEY), code 3 (KEY_2)";
+    static KEY_3 = "(EV_KEY), code 4 (KEY_3)";
+    static KEY_4 = "(EV_KEY), code 5 (KEY_4)";
+    static KEY_5 = "(EV_KEY), code 6 (KEY_5)";
+    static KEY_6 = "(EV_KEY), code 7 (KEY_6)";
+    static KEY_7 = "(EV_KEY), code 8 (KEY_7)";
+    static KEY_8 = "(EV_KEY), code 9 (KEY_8)";
+    static KEY_9 = "(EV_KEY), code 10 (KEY_9)";
+    static KEY_0 = "(EV_KEY), code 11 (KEY_0)";
+
+    /*number pad keys*/
+    static KEY_KP1 = "(EV_KEY), code 79 (KEY_KP1)";
+    static KEY_KP2 = "(EV_KEY), code 80 (KEY_KP2)";
+    static KEY_KP3 = "(EV_KEY), code 81 (KEY_KP3)";
+    static KEY_KP4 = "(EV_KEY), code 82 (KEY_KP4)";
+    static KEY_KP5 = "(EV_KEY), code 83 (KEY_KP5)";
+    static KEY_KP6 = "(EV_KEY), code 77 (KEY_KP6)";
+    static KEY_KP7 = "(EV_KEY), code 71 (KEY_KP7)";
+    static KEY_KP8 = "(EV_KEY), code 72 (KEY_KP8)";
+    static KEY_KP9 = "(EV_KEY), code 73 (KEY_KP9)";
+    static KEY_KP0 = "(EV_KEY), code 82 (KEY_KP0)";
+
+    /*alphabetic keys*/
+    static KEY_Q = "(EV_KEY), code 16 (KEY_Q)";
+    static KEY_W = "(EV_KEY), code 17 (KEY_W)";
+    static KEY_E = "(EV_KEY), code 18 (KEY_E)";
+    static KEY_R = "(EV_KEY), code 19 (KEY_R)";
+    static KEY_T = "(EV_KEY), code 20 (KEY_T)";
+    static KEY_Y = "(EV_KEY), code 21 (KEY_Y)";
+    static KEY_U = "(EV_KEY), code 22 (KEY_U)";
+    static KEY_I = "(EV_KEY), code 23 (KEY_I)";
+    static KEY_O = "(EV_KEY), code 24 (KEY_O)";
+    static KEY_P = "(EV_KEY), code 25 (KEY_P)";
+
+    static KEY_A = "(EV_KEY), code 30 (KEY_A)";
+    static KEY_S = "(EV_KEY), code 31 (KEY_S)";
+    static KEY_D = "(EV_KEY), code 32 (KEY_D)";
+    static KEY_F = "(EV_KEY), code 33 (KEY_F)";
+    static KEY_G = "(EV_KEY), code 34 (KEY_G)";
+    static KEY_H = "(EV_KEY), code 35 (KEY_J)";
+    static KEY_J = "(EV_KEY), code 36 (KEY_H)";
+    static KEY_K = "(EV_KEY), code 37 (KEY_K)";
+    static KEY_L = "(EV_KEY), code 38 (KEY_L)";
+
+    static KEY_Z = "(EV_KEY), code 44 (KEY_Z)";
+    static KEY_X = "(EV_KEY), code 45 (KEY_X)";
+    static KEY_C = "(EV_KEY), code 46 (KEY_C)";
+    static KEY_V = "(EV_KEY), code 47 (KEY_V)";
+    static KEY_B = "(EV_KEY), code 48 (KEY_B)";
+    static KEY_N = "(EV_KEY), code 49 (KEY_N)";
+    static KEY_M = "(EV_KEY), code 50 (KEY_M)";
+
+    static KEY_SEMICOLON = "(EV_KEY), code 39 (KEY_SEMICOLON)";
+    static KEY_APOSTROPHE = "(EV_KEY), code 40 (KEY_APOSTROPHE)";
+    static KEY_GRAVE = "(EV_KEY), code 41 (KEY_GRAVE)";
+    static KEY_LEFTSHIFT = "(EV_KEY), code 42 (KEY_LEFTSHIFT)";
+    static KEY_BACKSLASH = "(EV_KEY), code 43 (KEY_BACKSLASH)";
+
+    static KEY_MINUS = "(EV_KEY), code 12 (KEY_MINUS)";
+    static KEY_EQUAL = "(EV_KEY), code 13 (KEY_EQUAL)";
+    static KEY_BACKSPACE = "(EV_KEY), code 14 (KEY_BACKSPACE)";
+    static KEY_TAB = "(EV_KEY), code 15 (KEY_TAB)";
+    static KEY_LEFTBRACE = "(EV_KEY), code 26 (KEY_LEFTBRACE)";
+    static KEY_RIGHTBRACE = "(EV_KEY), code 27 (KEY_RIGHTBRACE)";
+    static KEY_ENTER = "(EV_KEY), code 28 (KEY_ENTER)";
+    static KEY_LEFTCTRL = "(EV_KEY), code 30 (KEY_LEFTCTRL)";
+
+    static KEY_KPDOT = "(EV_KEY), code 83 (KEY_KPDOT)";
+    static KEY_KPENTER = "(EV_KEY), code 96 (KEY_KPENTER)";
+    static KEY_RIGHTCTRL = "(EV_KEY), code 97 (KEY_RIGHTCTRL)";
+    static KEY_KPSLASH = "(EV_KEY), code 98 (KEY_KPSLASH)";
+    static KEY_RIGHTALT = "(EV_KEY), code 100 (KEY_RIGHTALT)";
+    static KEY_KPMINUS = "(EV_KEY), code 74 (KEY_KPMINUS)";
+    static KEY_KPPLUS = "(EV_KEY), code 75 (KEY_KPPLUS)";
+    static KEY_HOME = "(EV_KEY), code 102 (KEY_HOME)";
+    static KEY_UP = "(EV_KEY), code 103 (KEY_UP)";
+    static KEY_PAGEUP = "(EV_KEY), code 104 (KEY_PAGEUP)";
+    static KEY_PAGEDOWN = "(EV_KEY), code 109 (KEY_PAGEDOWN)";
+    static KEY_LEFT = "(EV_KEY), code 105 (KEY_LEFT)";
+    static KEY_RIGHT = "(EV_KEY), code 106 (KEY_RIGHT)";
+    static KEY_END = "(EV_KEY), code 107 (KEY_END)";
+    static KEY_DOWN = "(EV_KEY), code 108 (KEY_DOWN)";
+    static KEY_KPCOMMA = "(EV_KEY), code 121 (KEY_KPCOMMA)";
+    static KEY_LEFTMETA = "(EV_KEY), code 125 (KEY_LEFTMETA)";
+    static KEY_RIGHTMETA = "(EV_KEY), code 126 (KEY_RIGHTMETA)";
+    static KEY_INSERT = "(EV_KEY), code 110 (KEY_INSERT)";
+    static KEY_DELETE = "(EV_KEY), code 111 (KEY_DELETE)";
+
+    static KEY_KPASTERISK = "(EV_KEY), code 55 (KEY_KPASTERISK)";
+
+    static KEY_SPACE = "(EV_KEY), code 57 (KEY_SPACE)";
+    static KEY_SLASH = "(EV_KEY), code 53 (KEY_SLASH)";
+    static KEY_RIGHTSHIFT = "(EV_KEY), code 54 (KEY_RIGHTSHIFT)";
+    static KEY_LEFTALT = "(EV_KEY), code 56 (KEY_LEFTALT)";
+    static KEY_DOT = "(EV_KEY), code 52 (KEY_DOT)";
+    static KEY_COMMA = "(EV_KEY), code 51 (KEY_COMMA)";
+    static KEY_CAPSLOCK = "(EV_KEY), code 58 (KEY_CAPSLOCK)";
+
+    static KEY_NUMLOCK = "(EV_KEY), code 69 (KEY_NUMLOCK)";
+    static KEY_SCROLLOCK = "(EV_KEY), code 70 (KEY_SCROLLOCK)";
+
+    //pipe (alt modifier ?)
+    static KEY_102ND = "(EV_KEY), code 86 (KEY_102ND)";
+
+    static initKeys() {
+        DomQuery.querySelectorAll("button[data-key-code]").each(item => {
+            registerEventHandler(item.id.value, "click", "keybd1", KeyCodes[item.attr("data-key-code").value] + ", value 1", ["retroarch", "MAME", "atari", "mister"]);
+        });
+    }
+
+}
+
+/*automatic registration triggered for everything which has a data-key-code*/
+setTimeout(() => KeyCodes.initKeys(), 500);
+
