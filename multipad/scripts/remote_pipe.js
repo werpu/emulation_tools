c09b92fc7f312e68811749182f873a7bd7b5c728
Werner Punz <werner.punz@gmail.com>
2020-04-16 15:32:56 +0200

    implement layer switching

--- multipad/scripts/remote_pipe.js
+++ multipad/scripts/remote_pipe.js
@@ -1,15 +1,64 @@
-
-
 const client = new net.Socket();
 
 function connect() {
-    client.connect(9002, '10.0.0.50', function () {
+    client.connect(9002, '10.0.1.50', function () {
         console.log('Connected');
     });
 }
 
+function toggleLayers(currentLayer, nextLayer) {
+    currentLayer.addClass("hidden");
+    nextLayer.removeClass("hidden");
+}
+
+/**
+ * layer switcher
+ *
+ * allows to swith from one to another layer
+ * @param the element id which has to react
+ * @param fromLayer from layer style marker
+ * @param toLayer to layer style marker
+ * @param isStatic static means... that it is toggled permanently on click, otherwise it is only activated until released
+ */
+function registerLayerSwitch(id, fromLayer, toLayer, isStatic) {
+    const element = DomQuery.byId(id);
+    const fromLayerEl = DomQuery.querySelectorAll("." + fromLayer);
+    const fromLayerExists = !!fromLayerEl.length;
+    const toLayerEl = DomQuery.querySelectorAll("." + toLayer);
+    const toLayerExists = !!toLayerEl.length;
+    let shifted = false;
+
+    if (isStatic) {
+        element.addEventListener("click", (el) => {
+            if (!shifted) {
+                shifted = true;
+                DomQuery.byId(el.target).addClass("toggled");
+                toggleLayers(fromLayerEl, toLayerEl);
+            } else if (toLayerExists) {
+                shifted = false;
+                DomQuery.byId(el.target).removeClass("toggled");
+                toggleLayers(toLayerEl, fromLayerEl);
+            }
+        });
+    } else {
+        element.addEventListener("mousedown", (el) => {
+            shifted = true;
+
+            toggleLayers(fromLayerEl, toLayerEl);
+        });
+
+         element.addEventListener("mouseup", () => {
+            shifted = false;
+            toggleLayers(toLayerEl, fromLayerEl);
+         });
+
+    }
+
+}
+
+
 function registerEventHandler(id, id_evt, target, event, windowPattern, longRun, additionalExecute) {
-    document.getElementById(id).addEventListener("click", () => {
+    DomQuery.byId(id).addEventListener("click", () => {
         focus(windowPattern);
 
         if (client.destroyed) {
@@ -19,10 +68,10 @@ function registerEventHandler(id, id_evt, target, event, windowPattern, longRun,
             JSON.stringify({
                 to: target,
                 event: event,
-                long:  "" + !!longRun
+                long: "" + !!longRun
             })
         ].join(" "));
-        if(additionalExecute) {
+        if (additionalExecute) {
             additionalExecute();
         }
         setTimeout(() => focus(["multipad"]), 1000)
